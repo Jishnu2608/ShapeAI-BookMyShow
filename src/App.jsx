f9870168a020b690f2f657b1b263bbf8b712ea28
Jishnu2608 <jishnudeepborah@gmail.com>
2021-07-14 01:26:17 +0530

    tailwind css setup complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 function App() {
-  return <div className="App"></div>
+  return <div className="App p-10">
+    <h1 className="text-8xl">Hello</h1>
+    <h1 className="text-6xl hover:text-blue-600">Hello people</h1>
+    <button className="border-2 px-2 py-1 border-blue-500 
+    md:border-red-500 md:bg-transparent rounded 
+    bg-blue-500 text-white hover:bg-transparent hover:text-blue-600 hover:border-2 
+    focus:outline-none outline-none focus:ring focus:border-blue-300">Hello</button>
+  </div>
+  
 }
 
 export default App;
