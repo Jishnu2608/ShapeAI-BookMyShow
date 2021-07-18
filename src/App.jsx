3b3fc366dc13ea26dae8db4bd4da9451da503559
Jishnu2608 <jishnudeepborah@gmail.com>
2021-07-18 11:35:51 +0530

    playfilters

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {Route} from "react-router-dom";
+import { Route } from "react-router-dom";
 
 // HOC
 import DefaultHOC from "./HOC/Default.HOC";
@@ -7,6 +7,7 @@ import MovieHOC from "./HOC/Movie.HOC";
 // Pages
 import HomePage from "./pages/Home.page";
 import Movie from "./pages/Movie.page";
+import Plays from "./pages/Plays.page";
 
 // Import css files
 import "slick-carousel/slick/slick.css";
@@ -17,6 +18,7 @@ function App() {
     <>
       <DefaultHOC path="/" exact component={HomePage} />
       <MovieHOC path="/movie/:id" exact component={Movie} />
+      <DefaultHOC path="/plays" exact component={Plays} />
     </>
   );
 }
